c5f95839195b6d7a1770458f4f0f321b155808a0
mrayush011 <ayushkrchourasiya@gmail.com>
2025-06-01 01:04:18 +0530

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,17 @@ import { navItems } from "@/data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
+
 export default function Home() {
   return (
    <main className = "relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
     <div className = "max-w-7xl w-full">
       <FloatingNav navItems={navItems}/>
       <Hero />
-      <Grid />
+      {/* <Grid /> */}
       <RecentProjects />
       <Experience />
-      {/* <Approach /> */}
+      <Approach />
       <Footer />
     </div>
    </main>
